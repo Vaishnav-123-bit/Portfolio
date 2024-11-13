6005fcaeae9a67dd51fff295ef3dec26f3d49957
vaishnav mahdik <1032210456@tcetmumbai.in>
2024-11-13 15:59:15 +0530

    changes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -62,7 +62,7 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         {/* <p className={styles.sectionSubText}>what</p> */}
-        <h2 className={styles.sectionHeadText}>Work Experience !</h2>
+        <h2 className={styles.sectionHeadText}>Work Experience </h2>
       </motion.div>
 
       <div className="mt-20 flex flex-col">
