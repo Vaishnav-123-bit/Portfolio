91ac9e97bcdf22ccecd38e01f76e44d6dc8b9eef
vaishnav mahdik <1032210456@tcetmumbai.in>
2024-11-13 17:26:23 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ const App = () => {
       <div className='relative z-0 bg-primary'>
       <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
-         <div className="relative z-0"> <Hero /><StarsCanvas/></div>
+          <Hero/>
+         {/* <div className="relative z-0"> <Hero /><StarsCanvas/></div> */}
         </div>
       
         <About />
