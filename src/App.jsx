259e51794f2e0dec6b64db834561f3856b27a09d
vaishnav mahdik <1032210456@tcetmumbai.in>
2024-11-13 17:00:23 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-       <Navbar/>
-       <Hero/>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+      
         <About />
         <Experience />
         <Tech />
